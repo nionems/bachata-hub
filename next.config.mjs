9746825faa6439813d5fa9589454133d994031a8
lionel <nionems@icloud.com>
2025-04-03 23:51:29 +1100

    Update Next.js config to include googleapis in build

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), { 'googleapis': 'commonjs googleapis' }];
+    return config;
+  },
 }
 
 export default nextConfig
