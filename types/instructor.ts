2d3737121b59d28a28b7014875e6ab72bc09a67c
lionel <nionems@icloud.com>
2025-05-17 11:32:31 +1000

    Add Instructor type definition

---
+++ types/instructor.ts
@@ -0,0 +1,14 @@
+export interface Instructor {
+  id: string;
+  name: string;
+  location: string;
+  state: string;
+  imageUrl: string;
+  comment?: string;
+  danceStyles: string[];
+  emailLink?: string;
+  facebookLink?: string;
+  instagramLink?: string;
+  createdAt: string;
+  updatedAt: string;
+} 
