7cdebb4e1883ae183eedaa23294920a97c7ea65b
lionel <nionems@icloud.com>
2025-05-17 10:36:26 +1000

    Add DJ type definition

---
+++ types/dj.ts
@@ -0,0 +1,18 @@
+export interface Dj {
+  id: string;
+  name: string;
+  imageUrl?: string;
+  location: string;
+  city: string;
+  state: string;
+  description: string;
+  website?: string;
+  instagram?: string;
+  facebook?: string;
+  spotify?: string;
+  soundcloud?: string;
+  mixcloud?: string;
+  youtube?: string;
+  comment?: string;
+  googleMapLink?: string;
+} 
