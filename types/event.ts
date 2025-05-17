72c34809dd4a494a7642568e92c4d9990b9be8c1
lionel <nionems@icloud.com>
2025-05-17 10:57:23 +1000

    Add Event type definition

---
+++ types/event.ts
@@ -0,0 +1,17 @@
+export interface Event {
+  id: string;
+  name: string;
+  description: string;
+  date: string;
+  time: string;
+  location: string;
+  city: string;
+  state: string;
+  imageUrl: string;
+  ticketLink?: string;
+  facebookLink?: string;
+  instagramLink?: string;
+  websiteLink?: string;
+  createdAt: string;
+  updatedAt: string;
+} 
