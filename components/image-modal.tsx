2f2d30d7cf87b7c94aa4f8084a288c40afc41c86
lionel <nionems@icloud.com>
2025-06-04 12:53:19 +1000

    feat: add consistent close button to image modal

--- components/image-modal.tsx
+++ components/image-modal.tsx
@@ -1,5 +1,6 @@
 import { Dialog, DialogContent } from "@/components/ui/dialog"
 import Image from "next/image"
+import { X } from "lucide-react"
 
 interface ImageModalProps {
   isOpen: boolean
@@ -12,6 +13,12 @@ export function ImageModal({ isOpen, onClose, imageUrl, title }: ImageModalProps
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="max-w-4xl w-full p-0 bg-transparent border-none">
+        <button
+          onClick={onClose}
+          className="absolute top-4 right-4 text-white hover:text-gray-300 transition-colors z-50"
+        >
+          <X className="h-8 w-8" />
+        </button>
         <div className="relative w-full h-[80vh]">
           <Image
             src={imageUrl}
