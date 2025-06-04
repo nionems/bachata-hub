9a866a3b70c8fff5341272d22acbdadb4af7c001
lionel <nionems@icloud.com>
2025-06-04 15:42:41 +1000

    chore: add HSTS header for enhanced security

--- next.config.js
+++ next.config.js
@@ -43,6 +43,23 @@ const nextConfig = {
       },
     ]
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
